da735ffe6ba8e4daa5bab9c5e4dc59ac08f68e81
“Celeste45” <FallingLeaf42@gmail.com>
2023-11-09 12:16:52 +0800

    Read accelerometer data and heading

--- robotic_car/driver/magnetnometer/magnetnometer.c
+++ robotic_car/driver/magnetnometer/magnetnometer.c
@@ -1,50 +1,88 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include <math.h>
 
-// Configuration Constants
-#define I2C_ID i2c0 // I2C peripheral ID
-#define CLOCK_FREQUENCY 100000 // Clock frequency for I2C communication
-#define I2C_SDA_PIN 0 // GPIO pin for I2C SDA
-#define I2C_SCL_PIN 1 // GPIO pin for I2C SCL
-#define MAG_ADDR 0x1E // I2C address of the magnetometer
+#define I2C_PORT i2c0
+#define I2C_CLOCK_FREQ 100000
+#define I2C_SDA_PIN 0 // GPIO pin for I2C Serial Data (SDA)
+#define I2C_SCL_PIN 1 // GPIO pin for I2C Serial Clock (SCL)
 
-// Magnetometer Data Structure
+#define ACC_ADDR 0x19 // I2C address of accelerometer
+#define MAG_ADDR 0x1E // I2C address of magnetometer
+
+// Register address of accelerometer
+#define ACC_CTRL_REG1 0x20 // Control Register 1
+#define ACC_X_LSB 0x28 // Output Register X (LSB)
+#define ACC_X_MSB 0x29 // Output Register X (MSB)
+#define ACC_Y_LSB 0x2A // Output Register Y (LSB)
+#define ACC_Y_MSB 0x2B // Output Register Y (MSB)
+#define ACC_Z_LSB 0x2C // Output Register Z (LSB)
+#define ACC_Z_MSB 0x2D // Output Register Z (MSB)
+
+// Register address of magnetometer
+#define MAG_MR_REG 0x02 // Control Register MR
+#define MAG_X_LSB 0x04 // Output Register X (LSB)
+#define MAG_X_MSB 0x03 // Output Register X (MSB)
+#define MAG_Y_LSB 0x08 // Output Register Y (LSB)
+#define MAG_Y_MSB 0x07 // Output Register Y (MSB)
+#define MAG_Z_LSB 0x06 // Output Register Z (LSB)
+#define MAG_Z_MSB 0x05 // Output Register Z (MSB)
+
 typedef struct {
-    int16_t x; // X-axis data
-    int16_t y; // Y-axis data
-    int16_t z; // Z-axis data
-} MagnetometerData;
+    int16_t x, y, z;
+} Acc_Data;
+
+typedef struct {
+    int16_t x, y, z;
+} Mag_Data;
+
+void configure(uint8_t addr, uint8_t reg, uint8_t value) {
+    uint8_t data[] = {reg, value};
+    i2c_write_blocking(I2C_PORT, addr, data, 2, false);
+}
+
+uint8_t read(uint8_t addr, uint8_t reg) {
+    uint8_t data;
+    i2c_write_blocking(I2C_PORT, addr, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, addr, &data, 1, false);
+    return data;
+}
 
 int main() {
-    // Initialize standard I/O
     stdio_init_all();
-    
-    // Initialize I2C and configure GPIO pins for I2C communication
-    i2c_init(I2C_ID, CLOCK_FREQUENCY);
+    i2c_init(I2C_PORT, I2C_CLOCK_FREQ);
+    i2c_set_slave_mode(I2C_PORT, false, 0);
     gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
 
-    // Configure the magnetometer for continuous conversion mode
-    uint8_t config[] = {0x00}; // Mode selection (continuous-conversion)
-    i2c_write_blocking(I2C_ID, MAG_ADDR, config, sizeof(config), false);
-    sleep_ms(3000); // Delay to allow the magnetometer to stabilize
+    configure(ACC_ADDR, ACC_CTRL_REG1, 0x57);
+    configure(MAG_ADDR, MAG_MR_REG, 0x00);
+    sleep_ms(3000); // Allow time for sensor initialization
 
     while (true) {
-        // Read magnetometer data
-        uint8_t data[6];
-        i2c_read_blocking(I2C_ID, MAG_ADDR, data, sizeof(data), false);
+        // Extract and format the accelerometer data
+        Acc_Data acc_data;
+        acc_data.x = (int16_t)((read(ACC_ADDR, ACC_X_MSB) << 8) | read(ACC_ADDR, ACC_X_LSB));
+        acc_data.y = (int16_t)((read(ACC_ADDR, ACC_Y_MSB) << 8) | read(ACC_ADDR, ACC_Y_LSB));
+        acc_data.z = (int16_t)((read(ACC_ADDR, ACC_Z_MSB) << 8) | read(ACC_ADDR, ACC_Z_LSB));
 
         // Extract and format the magnetometer data
-        MagnetometerData magnetometerData;
-        magnetometerData.x = (int16_t)((data[1] << 8) | data[0]) >> 4;
-        magnetometerData.y = (int16_t)((data[3] << 8) | data[2]) >> 4;
-        magnetometerData.z = (int16_t)((data[5] << 8) | data[4]) >> 4;
+        Mag_Data mag_data;
+        mag_data.x = (int16_t)((read(MAG_ADDR, MAG_X_MSB) << 8) | read(MAG_ADDR, MAG_X_LSB));
+        mag_data.y = (int16_t)((read(MAG_ADDR, MAG_Y_MSB) << 8) | read(MAG_ADDR, MAG_Y_LSB));
+        mag_data.z = (int16_t)((read(MAG_ADDR, MAG_Z_MSB) << 8) | read(MAG_ADDR, MAG_Z_LSB));
 
-        // Print the magnetometer data
-        printf("Magnetometer Data - X: %d, Y: %d, Z: %d\n", magnetometerData.x, magnetometerData.y, magnetometerData.z);
+        float heading = atan2(mag_data.y, mag_data.x) * 180 / M_PI;
 
-        sleep_ms(1000); // Delay between successive readings
+        if (heading < 0)
+            heading += 360;
+
+        printf("Accelerometer x: %d y: %d z: %d\n", acc_data.x, acc_data.y, acc_data.z);
+        printf("Magnetometer x: %d y: %d z: %d\n", mag_data.x, mag_data.y, mag_data.z);
+        printf("Heading: %.2f\n\n", heading);
+
+        sleep_ms(200);
     }
 
     return 0;
