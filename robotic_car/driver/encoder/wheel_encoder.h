5c97465bf1f6a8217cdb76adc9f2e1f0f4752909
lokjunneo <raizutiamat@gmail.com>
2023-11-17 21:50:25 +0800

    Fixed speed calculation in wheel encoder

--- robotic_car/driver/encoder/wheel_encoder.h
+++ robotic_car/driver/encoder/wheel_encoder.h
@@ -39,8 +39,8 @@ void wheel_moved_isr(uint gpio, uint32_t events) {
     struct wheel_encoder * encoder = gpio == data->left_encoder.pin ? &data->left_encoder : &data->right_encoder;
 
     encoder->ticks++;
-    encoder->total_distance = (float)encoder->ticks / 40 * 33.2f;
-    encoder->current_speed = 33.2f / 40 / ((float)(current_time - encoder->last_time) / 1000000.0f);
+    encoder->total_distance = (float)encoder->ticks / 40 * 20.4f;//33.2f;
+    encoder->current_speed = /* 33.2f */ 20.4f / 40 / ((float)(current_time - encoder->last_time) / 1000000.0f);
     encoder->last_time = current_time;
 
     printf("Encoder: %s, Distance: %.2f cm, Speed: %.2f cm/s\n", encoder->side, encoder->total_distance, encoder->current_speed);
